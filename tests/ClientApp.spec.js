c1bfd67a43aaa944452f643239ed30cf7a1a37b2
nauman-ahmed07 <nauman.ahmed07.na@gmail.com>
2026-02-04 19:11:10 +0100

    added code to get the details from the cart screen

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,18 +1,38 @@
-const {test, expect} = require('@playwright/test');
-const { text } = require('node:stream/consumers');
+const { test, expect } = require('@playwright/test');
 
-test('Client App Login',async ({browser})=>
-{
+test('Client App Login', async ({ browser }) => {
 
-    const products = page.locator(".card-body");
-    await page.goto("https://rahulshettyacademy.com/client/#/auth/login"); 
-    const userEmail = page.locator('#userEmail');
-    const login = page.locator('#login');
-    await userEmail.fill("nauman.ahmed07.na@gmail.com");
-    await page.locator('#userPassword').fill("Password123");
-    await login.click();
-    await page.locator(".card-body b").first().waitFor();
-    const allTitles = await cardTitles.allTextContents();
-    console.log(allTitles);
-//test2
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  const productName = "ZARA COAT 3";
+
+  await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+
+  await page.locator("#userEmail").fill("nauman.ahmed07.na@gmail.com");
+  await page.locator("#userPassword").fill("Password123");
+  await page.locator("#login").click();
+
+  await page.locator(".card-body b").first().waitFor();
+
+  const products = page.locator(".card-body");
+  const count = await products.count();
+
+  for (let i = 0; i < count; i++) {
+    const title = await products.nth(i).locator("b").textContent();
+
+    if (title.trim() === productName) {
+      await products.nth(i).locator("text=Add To Cart").click();
+      break;
+    }
+  }
+  await page.locator("[routerlink*='/cart']").click();
+  await page.locator("div li").first().waitFor();
+  const bool =  page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+  expect(bool).toBeTruthy();
+
+
+
+
+  
 });
