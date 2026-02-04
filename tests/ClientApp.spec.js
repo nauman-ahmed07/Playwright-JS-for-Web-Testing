02e3f9f51dd4b6516a113e2dbd44088abc93c95f
nauman-ahmed07 <nauman.ahmed07.na@gmail.com>
2026-02-04 16:49:28 +0100

    code updated in ClientApp.spec.js

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,21 +1,16 @@
 const {test, expect} = require('@playwright/test');
 const { text } = require('node:stream/consumers');
 
-test('Browser Context Playwright test',async ({browser})=>
+test('Client App Login',async ({browser})=>
 {
 
-    const context = await browser.newContext();
-    const page = await context.newPage();
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login"); 
-    console.log(await page.title());
-    //CSS
     const userEmail = page.locator('#userEmail');
     const login = page.locator('#login');
     await userEmail.fill("nauman.ahmed07.na@gmail.com");
     await page.locator('#userPassword').fill("Password123");
     await login.click();
-    const cardTitles =  page.locator(".card-body b");
-    //console.log(await page.locator("#error").textContent());
     await page.locator(".card-body b").first().waitFor();
     const allTitles = await cardTitles.allTextContents();
     console.log(allTitles);
