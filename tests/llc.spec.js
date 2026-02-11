8f7334bb0a52431a16d815b806ecfcdaf681bf32
nauman-ahmed07 <nauman.ahmed07.na@gmail.com>
2026-02-11 17:14:01 +0100

    Added llc.spec test

---
+++ tests/llc.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('Playwright Special Locators', async ({ page }) => {
+
+await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+
+});
