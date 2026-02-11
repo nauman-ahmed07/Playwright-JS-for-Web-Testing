b371ea2de8c59d9e9c4d2499ba80915567bc9907
nauman-ahmed07 <nauman.ahmed07.na@gmail.com>
2026-02-11 18:51:49 +0100

    Added different Locators in the llc.spec.js file

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -3,6 +3,14 @@ const { test, expect } = require('@playwright/test');
 test('Playwright Special Locators', async ({ page }) => {
 
 await page.goto("https://rahulshettyacademy.com/angularpractice/");
+await page.getByLabel("Check me out if you Love IceCreams!").check();
+await page.getByLabel("Gender").selectOption("Male");
+await page.getByLabel("Employed").click();
+await page.getByPlaceholder("Password").fill("abc123");
+await page.getByRole("button", {name: 'Submit'}).click();
+await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+await page.getByRole("Link", {name:'Shop'}).click();
+await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
 
 
 });
